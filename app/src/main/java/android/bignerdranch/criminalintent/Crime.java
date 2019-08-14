e026d0d0e95a912d7dc26a45b83e907621dfcb7a
usc-pmk003 <pmk003@student.usc.edu.au>
2019-08-14 16:51:47 +1000

    Initial Commit.

---
+++ app/src/main/java/android/bignerdranch/criminalintent/Crime.java
@@ -0,0 +1,46 @@
+package android.bignerdranch.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    public UUID getId() {
+        return mId;
+    }
+
+    private UUID mId;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+}
